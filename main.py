86f0316b815e7287f0784d8424176178faad99c4
leitosama <fs.leito@gmail.com>
2023-08-05 00:59:02 +0700

    logging added

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import hmac
 from hashlib import sha256
 import requests
 import json
+import logging
 
 
 def _verify_signature(secret: bytes, sig: str, msg: bytes) -> bool:
     mac = hmac.new(secret, msg=msg, digestmod=sha256)
-    return hmac.compare_digest(mac.hexdigest(), sig)
+    macdigest = mac.hexdigest()
+    logging.debug(f"Compare digest {macdigest} and sig {sig}")
+    return hmac.compare_digest(macdigest, sig)
 
 
 def createMessage(gh_event: str, body: str) -> str:
@@ -16,7 +19,9 @@ def createMessage(gh_event: str, body: str) -> str:
         searchpath=os.environ['TEMPLATES_PATH'])
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(f'{gh_event}.j2')
-    return template.render(data=json.loads(body))
+    text = template.render(data=json.loads(body))
+    logging.debug(f"Event: {gh_event}, text: {text}")
+    return text
 
 
 def sendMessage(text: str) -> dict:
@@ -26,7 +31,7 @@ def sendMessage(text: str) -> dict:
         "chat_id": os.environ['CHAT_ID'],
         "reply_to_message_id": os.environ.get('THREAD_ID','')
     }
-    print(payload)
+    logging.debug(f"TG Payload: {payload}")
     headers = {
         "accept": "application/json",
         "content-type": "application/json"
@@ -34,28 +39,36 @@ def sendMessage(text: str) -> dict:
     r = requests.post(
         f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}/sendMessage', json=payload, headers=headers)
     if r.status_code != 200:
-        return {'statusCode': r.status_code, 'body': r.text}
+        err = {'statusCode': r.status_code, 'body': r.text}
+        logging.error(err)
+        return err
     return {'statusCode': 200, 'body': 'Translated to recievers'}
 
 # Special function to YC Cloud Functions
 
+def returnHandler(resp):
+    logging.info(resp)
+    return resp
 
 def ya_handler(event, context):
+    logging.getLogger().setLevel(logging.DEBUG)
     if event['httpMethod'] != "POST":
-        return {'statusCode': 405}
+        logging.debug(f"HTTP Method is {event['httpMethod']}")
+        return returnHandler({'statusCode': 405})
     if not "GitHub-Hookshot" in event['headers']['User-Agent']:
-        return {'statusCode': 403}
+        logging.debug(f"User-Agent is {event['headers']['User-Agent']}")
+        return returnHandler({'statusCode': 403})
     if 'WEBHOOK_SECRET' in os.environ:
         if not 'X-Hub-Signature-256' in event['headers']:
-            return {'statusCode': 403, 'body': 'Webhook signature is missing'}
+            return returnHandler({'statusCode': 403, 'body': 'Webhook signature is missing'})
         gh_signature = event['headers']['X-Hub-Signature-256']
         if not _verify_signature(
             os.environ['WEBHOOK_SECRET'].encode('utf-8'),
             gh_signature.split('=')[1],
             event['body'].encode('utf-8')
         ):
-            return {'statusCode': 403, 'body': 'Webhook signature is wrong'}
+            return returnHandler({'statusCode': 403, 'body': 'Webhook signature is wrong'})
     if not ('CHAT_ID' in os.environ or 'BOT_TOKEN' in os.environ or 'TEMPLATES_PATH' in os.environ):
-        return {'statusCode': 500, 'body': 'Some settings is missing'}
+        return returnHandler({'statusCode': 500, 'body': 'Some settings is missing'})
     text = createMessage(event['headers']['X-Github-Event'], event['body'])
-    return sendMessage(text)
+    return returnHandler(sendMessage(text))
